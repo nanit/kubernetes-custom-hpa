44c134720b816ecd6e89e5de750be544a7533197
ytmlndr <ytmlndr@gmail.com>
2020-10-05 13:01:52 +0300

    exit when required inputs are missing

--- app/src/custom_hpa/core.clj
+++ app/src/custom_hpa/core.clj
@@ -6,13 +6,20 @@
             [custom-hpa.metric.core :as metric])
   (:gen-class))
 
+(defn- check-args [deployment deployment-namespace]
+  (when (or (nil? deployment)
+            (nil? deployment-namespace))
+    (logger/error "Required inputs are missing, deployment =" deployment "namespace =" deployment-namespace)
+    (System/exit 1)))
+
 (defn -main
   "Initializes core components and starts the custom HPA"
   [& args]
   (let [deployment (System/getenv "DEPLOYMENT")
         deployment-namespace (System/getenv "NAMESPACE")]
-    (logger/info "Custom HPA started")
     (logger/set-level! (keyword (or (System/getenv "LOG_LEVEL") "info")))
     (logger/swap-config! assoc-in [:timestamp-opts :pattern] "yyyy-MM-dd'T'HH:mm:ss.SSSZ")
+    (logger/info "Custom HPA started")
+    (check-args deployment deployment-namespace)
     (web-server/start (or (int-env "PORT") 3000))
     (control-loop/start deployment deployment-namespace (metric/default-provider))))
